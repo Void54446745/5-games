c1b2f9f8ba45b67e437a148f16d53abb113037af
Void <me5634524@gmail.com>
2026-03-29 19:27:14 +0100

    Update main.py

--- space shooter/code_/main.py
+++ space shooter/code_/main.py
@@ -9,26 +9,31 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 running = True
 pygame.display.set_caption('Space ship game')
-left = False
+Clock = pygame.time.Clock()
+
 # Surface
 surf = pygame.Surface((100, 200))
 surf.fill('BlueViolet')
 x = 100
 
-# Import an image
+# Imports
 
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = pygame.math.Vector2(2, -1)
+player_speed = 10
+
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(random.randint(0, WINDOW_WIDTH), random.randint(0, WINDOW_HEIGHT)) for i in range (20)]
+
 meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
 meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
-laser_rect = laser_surf.get_frect(bottomright = (WINDOW_WIDTH -10, WINDOW_HEIGHT - 10))
-# Stars
 
+laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 while running:
+    Clock.tick(10)
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -39,15 +44,10 @@ while running:
     for pos in star_positions:
         screen.blit(star_surf, pos)
     screen.blit(meteor_surf, meteor_rect)
-    if (player_rect.right < WINDOW_WIDTH) and (left == False):
-        player_rect.left += 0.2
-    elif (player_rect.left >= 0) and (left == True):
-        player_rect.left -= 0.2
-    elif left == False:
-        left = True
-    elif left == True:
-        left = False
     screen.blit(laser_surf, laser_rect)
+
+    # player movement
+    player_rect.center += player_direction * player_speed
     screen.blit(player_surf, player_rect)
     pygame.display.update()
 
